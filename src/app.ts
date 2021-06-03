85bddb1af054d8d2027f0c60460fafc8fcc0c512
REAZZY <maxim.nebela@gmail.com>
2021-06-03 10:54:03 +0300

    feat: add uncaughtException and unhandledRejection events logger

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,23 @@ const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
-const {logRequestMiddleware, catchErrorMiddleware} = require('./middlewares/middlewares');
+const {logRequestMiddleware, catchErrorMiddleware,errLogger} = require('./middlewares/middlewares');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/boards.router');
 const tasksRouter = require('./resources/tasks/tasks.router');
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+
+process.on("uncaughtException", (err:Error, origin:string)=> {
+  errLogger("uncaughtException",err,origin)
+  process.exit(1)
+})
+process.on("unhandledRejection", (err:Error,origin:string)=> {
+  errLogger("unhandledRejection",err,origin)
+  process.exit(1)
+})
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
