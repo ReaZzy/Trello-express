30ca37852abc754a26b7b6dd506937e442e1d988
REAZZY <maxim.nebela@gmail.com>
2021-06-05 10:42:54 +0300

    fix: change req.path to req.originalUrl

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
-const {logRequestMiddleware, catchErrorMiddleware,errLogger} = require('./middlewares/middlewares');
+const {logger} = require('./middlewares/middlewares');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/boards.router');
 const tasksRouter = require('./resources/tasks/tasks.router');
@@ -12,14 +12,7 @@ const tasksRouter = require('./resources/tasks/tasks.router');
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
-process.on("uncaughtException", (err:Error, origin:string)=> {
-  errLogger("uncaughtException",err,origin)
-  process.exit(1)
-})
-process.on("unhandledRejection", (err:Error,origin:string)=> {
-  errLogger("unhandledRejection",err,origin)
-  process.exit(1)
-})
+
 
 
 app.use(express.json());
@@ -34,13 +27,13 @@ app.use('/', (req:Request, res:Response, next:NextFunction) => {
   next();
 });
 
-app.all('*', logRequestMiddleware);
+app.use(logger.catchErrorMiddleware)
+app.all("*", logger.logRequestMiddleware)
 
 
 
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 app.use('/boards', tasksRouter);
-app.use(catchErrorMiddleware);
 
 module.exports = app;
